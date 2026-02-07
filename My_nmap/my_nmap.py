7513cf1610439d67991ad3758b2b88668b680c6d
LabreCoder <jvlabremachado@gmail.com>
2026-02-06 22:52:29 -0300

    Adicionando comentários

--- My_nmap/my_nmap.py
+++ My_nmap/my_nmap.py
@@ -1,6 +1,6 @@
 import socket
 
-host="bancocn.com"
+host="bancocn.com" # esse é um site que foi montado pelo  site da Solyd para ser usado em testes de segurança, não é ilegal fazer varreduras nesse site, mas é ilegal fazer varreduras em sites sem autorização, então use esse código apenas para fins educacionais e teste somente em sites que você tem autorização para testar.
 
 for p in range(1,1025):
     s=socket.socket()
